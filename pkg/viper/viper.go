055b21e678f87bd2686ac8fbb7696477e0937163
Shiyuan Ji <55011513+Centurybbx@users.noreply.github.com>
2023-08-07 21:25:16 +0800

    Basic web frame using hertz and kitex (#12)
    * first finish frame with hertz and kitex
    
    * test
    
    * fix conflicts

---
+++ pkg/viper/viper.go
@@ -0,0 +1,24 @@
+package viper
+
+import (
+	"github.com/spf13/viper"
+)
+
+var Config *viper.Viper
+
+func init() {
+}
+
+func LoadConfig(path string) *viper.Viper {
+	conf := viper.New()
+	conf.SetConfigFile(path)
+	conf.AddConfigPath("./")
+	conf.AddConfigPath("../")
+	conf.AddConfigPath("../../")
+
+	err := conf.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	return conf
+}
