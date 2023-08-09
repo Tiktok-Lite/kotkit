15d7f2bc4b208bb2c2085df59532f9f561ecd742
Shiyuan Ji <919745273@qq.com>
2023-08-09 10:51:03 +0800

    fix missing status_code when return userinfo

--- cmd/user/handler.go
+++ cmd/user/handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/Tiktok-Lite/kotkit/internal/repository"
 	"github.com/Tiktok-Lite/kotkit/kitex_gen/user"
+	"github.com/Tiktok-Lite/kotkit/pkg/helper/constant"
 	"github.com/Tiktok-Lite/kotkit/pkg/helper/converter"
 )
 
@@ -11,11 +12,21 @@ import (
 type UserServiceImpl struct{}
 
 // UserInfo implements the UserServiceImpl interface.
-func (s *UserServiceImpl) UserInfo(ctx context.Context, req *user.UserInfoRequest) (resp *user.UserInfoResponse, err error) {
+func (s *UserServiceImpl) UserInfo(ctx context.Context, req *user.UserInfoRequest) (*user.UserInfoResponse, error) {
 	// TODO: Your code here...
 	repo := repository.NewRepository(repository.DB)
 	userRepo := repository.NewUserRepository(repo)
 	usr, err := userRepo.QueryUserByID(req.UserId)
+
+	if err != nil {
+		// TODO: 添加日志
+		res := &user.UserInfoResponse{
+			StatusCode: constant.StatusErrorCode,
+			StatusMsg:  "query failed from database",
+			User:       nil,
+		}
+		return res, err
+	}
 
 	// 从数据库中查询到的user，转换为proto生成后的类型
 	userResp, err := converter.ConvertUserModelToProto(usr)
@@ -25,8 +36,8 @@ func (s *UserServiceImpl) UserInfo(ctx context.Context, req *user.UserInfoReques
 
 	// TODO(century): 这里只是测试用，还需完善
 	res := &user.UserInfoResponse{
-		StatusCode: 0,
-		StatusMsg:  nil,
+		StatusCode: constant.StatusOKCode,
+		StatusMsg:  "query user info success",
 		User:       userResp,
 	}
 
