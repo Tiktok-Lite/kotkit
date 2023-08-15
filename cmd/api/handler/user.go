1dcda27897e189d9252d26f0e2e2dd79b524cd9f
Shiyuan Ji <919745273@qq.com>
2023-08-15 22:39:16 +0800

    [Enhancement] refactor c.JSON using resp

--- cmd/api/handler/user.go
+++ cmd/api/handler/user.go
@@ -5,41 +5,35 @@ import (
 	"github.com/Tiktok-Lite/kotkit/cmd/api/rpc"
 	"github.com/Tiktok-Lite/kotkit/internal/response"
 	"github.com/Tiktok-Lite/kotkit/kitex_gen/user"
-	"github.com/Tiktok-Lite/kotkit/pkg/helper/constant"
+	"github.com/Tiktok-Lite/kotkit/pkg/log"
 	"github.com/cloudwego/hertz/pkg/app"
 	"net/http"
 	"strconv"
 )
 
 func UserInfo(ctx context.Context, c *app.RequestContext) {
+	logger := log.Logger()
+
 	userID := c.Query("user_id")
 	token := c.Query("token")
 	id, err := strconv.ParseInt(userID, 10, 64)
 	if err != nil {
-		// 先不处理，后面再做
+		logger.Errorf("failed to parse user_id: %v", err)
+		ResponseError(c, http.StatusBadRequest,
+			response.PackUserInfoError("请检查您的输入是否合法"))
 		return
 	}
 	req := &user.UserInfoRequest{
 		UserId: id,
 		Token:  token,
 	}
-	resp, _ := rpc.UserInfo(ctx, req)
-	if resp.StatusCode == constant.StatusOKCode {
-		c.JSON(http.StatusOK, response.UserInfo{
-			Base: response.Base{
-				StatusCode: constant.StatusOKCode,
-				StatusMsg:  resp.StatusMsg,
-			},
-			User: resp.User,
-		})
+	resp, err := rpc.UserInfo(ctx, req)
+	if err != nil {
+		logger.Errorf("failed to call rpc: %v", err)
+		ResponseError(c, http.StatusInternalServerError,
+			response.PackUserInfoError("用户信息获取失败，服务器内部问题"))
 		return
 	}
 
-	c.JSON(http.StatusOK, response.UserInfo{
-		Base: response.Base{
-			StatusCode: constant.StatusErrorCode,
-			StatusMsg:  "用户信息获取失败",
-		},
-		User: nil,
-	})
+	ResponseSuccess(c, response.PackUserInfoSuccess(resp.User, "用户信息获取成功"))
 }
--- internal/response/login.go
+++ internal/response/login.go
@@ -1,8 +1,25 @@
 package response
 
-
 type Login struct {
 	Base
-	UserID	int64         `json:"user_id"`
-	Token	string 		  `json:"token"`
+	UserID int64  `json:"user_id"`
+	Token  string `json:"token"`
+}
+
+func PackLoginOrRegisterSuccess(userID int64, token string, msg string) Login {
+	base := PackBaseSuccess(msg)
+	return Login{
+		Base:   base,
+		UserID: userID,
+		Token:  token,
+	}
+}
+
+func PackLoginOrRegisterError(errorMsg string) Login {
+	base := PackBaseError(errorMsg)
+	return Login{
+		Base:   base,
+		UserID: -1,
+		Token:  "",
+	}
 }
